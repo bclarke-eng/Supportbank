729eedd9232892b75bcf243158274cc599eaa8fb
PROBERT Beth <bprobert@altranuk.local>
2019-09-12 10:38:50 +0100

    Finished functions.

--- SB2.py
+++ SB2.py
@@ -6,6 +6,24 @@ from Transaction import Transaction
 logging.basicConfig(filename='SupportBank.log', filemode='w', level=logging.DEBUG)
 
 
+def valid_file():
+    logging.info("User was asked to input a file name.")
+    carry_on = True
+    amounts = []
+    transaction_list = []
+    while carry_on:
+        file_input = input("Please enter the name of the file you'd like to search (with extension):\n")
+        try:
+            transaction_list = read_file(file_input)  # user inputs a file to be read
+            amounts = Balances(transaction_list)
+            carry_on = False
+            enter_command()
+        except FileNotFoundError:
+            logging.info("The user entered an invalid file name. User entered: " + file_input)
+            print("Sorry. That file name is invalid.")
+    return amounts, transaction_list
+
+
 def read_file(filename):  # function that opens and reads a csv file
     with open(filename, "r") as file:
         if file.mode == "r":
@@ -23,6 +41,12 @@ def read_file(filename):  # function that opens and reads a csv file
             return transactions
             # data[0] = date
             # data[3] = circumstances
+
+
+def enter_command():
+    logging.info("User was asked to select a command.")
+    print("\nPlease type a command from the following list: \n"
+          "1. List All \n2. List Account name (e.g. List Account Jon A) \n3. Help \n4. Quit")
 
 
 def list_all(balances):  # function for listing all names and their net balances
@@ -50,7 +74,8 @@ def filter_transactions(account_name, transactions):
     logging.info(account_name + "'s account contained " + str(counter) + " transactions")
 
 
-def list_account(account_name, transactions):  # function that prints a list of all transactions and net balance for a
+def list_account(account_name, transactions, balances):  # function that prints a list of all transactions and net
+    # balance for a
     # person
     filter_transactions(account_name, transactions)
 
@@ -59,58 +84,37 @@ def list_account(account_name, transactions):  # function that prints a list of 
     logging.info("List account function was completed for " + account_name)
 
 
-logging.info("User was asked to input a file name.")
-transactions = []
-balances = []
-cont = True
-while cont:
-    file_input = input("Please enter the name of the file you'd like to search (with extension):\n")
-    try:
-        transactions = read_file(file_input)  # user inputs a file to be read
-        balances = Balances(transactions)
-        cont = False
-        # do something
-    except FileNotFoundError:
-        logging.info("The user entered an invalid file name. User entered: " + file_input)
-        print("Sorry. That file name is invalid.")
+def initiate_commands():
+    amounts, transaction_list = valid_file()
+    cont = True  # allows user to exit at some point
+    while cont:
+        command = input()
+        if command.lower() == "list all" or command.lower() == "listall":  # user
+            # chooses a command
+            logging.info("User selected the List All function.")
+            list_all(amounts)
+            enter_command()
+        elif re.search(r"[Ll]ist ?[Aa]ccount", command):  # checks that the user has entered the correct command
+            name = re.findall(r"[Ll]ist ?[Aa]ccount ?([A-Z][a-z]* ?[A-Z]?[a-z]*?)\b", command)[
+                0]  # finds name of account
+            # they want
+            print("Here are the transactions for " + name + ":\n")
+            list_account(name, transaction_list, amounts)
+            enter_command()
+        elif command.lower() == "help":  # help command brings up menu of commands
+            logging.info("User requested help. Help menu displayed.")
+            enter_command()
+        elif command.lower() == "quit":  # exits the loop
+            print("Thanks for visiting!")
+            logging.info("User quit the program.")
+            cont = False
+            logging.info("Program terminated.")
+            break
+        else:
+            print("Invalid Command\n")  # if a user enters an invalid command, the loop restarts
+            logging.info(
+                "User entered an invalid command and was asked to try again.  The command entered was: " + command)
+            enter_command()
 
-logging.info("User was asked to select a command.")
-print("Please type a command from the following list: \n"
-      "List All \nList Account name (e.g. List Account Jon A) \nhelp \nquit")
 
-cont = True  # allows user to exit at some point
-while cont:
-    command = input()
-    if command.lower() == "list all" or command.lower() == "listall":  # user
-        # chooses a command
-        logging.info("User selected the List All function.")
-        list_all(balances)
-        logging.info("User was asked to select a command.")
-        print("Please type a command from the following list: \n"
-              "List All \nList Account name (e.g. List Account Jon A) \nhelp \nquit")
-    elif re.search(r"[Ll]ist ?[Aa]ccount", command):  # checks that the user has entered the correct command
-        name = re.findall(r"[Ll]ist ?[Aa]ccount ?([A-Z][a-z]* ?[A-Z]?[a-z]*?)\b", command)[0]  # finds name of account
-        # they want
-        print("Here are the transactions for " + name + ":\n")
-        list_account(name, transactions)
-        logging.info("User was asked to select a command.")
-        print("Please type a command from the following list: \n"
-              "List All \nList Account name (e.g. List Account Jon A) \nhelp \nquit")
-    elif command == "help":  # help command brings up menu of commands
-        print("Please type a command from the following list: \n"
-              "List All \nList Account name (e.g. List Account Jon A) \nhelp \nquit")
-        logging.info("User requested help. Help menu displayed.")
-        logging.info("User was asked to select a command.")
-    elif command == "quit":  # exits the loop
-        print("Thanks for visiting!")
-        logging.info("User quit the program.")
-        cont = False
-        logging.info("Program terminated.")
-        break
-    else:
-        print("Invalid Command\n")  # if a user enters an invalid command, the loop restarts
-        print("Please type a command from the following list: \n"
-              "List All \nList Account name (e.g. List Account Jon A) \nhelp \nquit")
-        logging.info(
-            "User entered an invalid command and was asked to try again.  The command entered was: " + command)
-        logging.info("User was asked to select a command.")
+initiate_commands()
